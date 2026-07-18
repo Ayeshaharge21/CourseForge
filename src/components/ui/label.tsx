07e53fd26cdd7169976b5d5ddaa6c880bc8dba10
Ayeshaharge21 <ayeshaharge21@gmail.com>
2026-07-18 16:56:03 +0530

    Fix ref forwarding in Label component

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "../../lib/utils"
+import { cn } from "../../lib/utils" 
 
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
@@ -15,7 +15,7 @@ const Label = React.forwardRef<
     VariantProps<typeof labelVariants>
 >(({ className, ...props }, ref) => (
   <LabelPrimitive.Root
-    ref={ref}
+    ref={ref(ref)}
     className={cn(labelVariants(), className)}
     {...props}
   />
